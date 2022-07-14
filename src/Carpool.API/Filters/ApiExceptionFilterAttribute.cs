f24783ebb827c5873b0e9adbd03f0981097f5fc8
Ivan <ivan.kranjec95@gmail.com>
2022-07-15 01:07:15 +0200

    [Carpool.API] - add exception filter.

---
+++ src/Carpool.API/Filters/ApiExceptionFilterAttribute.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Carpool.API.Filters
+{
+    public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            HandleException(context);
+
+            base.OnException(context);
+        }
+
+        private void HandleException(ExceptionContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                HandleInvalidModelStateException(context);
+                return;
+            }
+
+            HandleUnknownException(context);
+        }
+
+
+        private void HandleUnknownException(ExceptionContext context)
+        {
+            ProblemDetails details = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = context.Exception.Message,
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+                Detail = context.Exception.StackTrace
+            };
+
+            context.Result = new ObjectResult(details)
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+            context.ExceptionHandled = true;
+        }
+
+        private void HandleInvalidModelStateException(ExceptionContext context)
+        {
+            ValidationProblemDetails details = new ValidationProblemDetails(context.ModelState)
+            {
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
+            };
+
+            context.Result = new BadRequestObjectResult(details);
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
