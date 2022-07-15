0b359afb373aaaccb78af0e058c585a33d4f0a41
Ivan <ivan.kranjec95@gmail.com>
2022-07-16 01:01:42 +0200

    [Carpool.Infrastructure] - add ConfigureServices class.

---
+++ src/Carpool.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,26 @@
+﻿using Carpool.Application.Common.Interfaces;
+using Carpool.Infrastructure.Persistance;
+using Carpool.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Carpool.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>
+                (options => options.UseNpgsql(configuration.GetSection("ConnectionStrings:DefaultConnection").Value));
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            services.AddScoped<ApplicationDbContextInitialiser>();
+
+            services.AddTransient<IDateTime, DateTimeService>();
+
+            return services;
+        }
+    }
+}
