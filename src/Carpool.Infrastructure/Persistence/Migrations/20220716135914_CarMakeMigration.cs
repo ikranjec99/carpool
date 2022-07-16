fdd4151827733a8fe259d54b5e57c2475a613ff2
Ivan <ivan.kranjec95@gmail.com>
2022-07-16 16:14:11 +0200

    [Carpool.Infrastructure] - add CarMake migration.

---
+++ src/Carpool.Infrastructure/Persistence/Migrations/20220716135914_CarMakeMigration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Carpool.Infrastructure.Persistence.Migrations
+{
+    public partial class CarMakeMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarMakes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarMakes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CarMakes",
+                columns: new[] { "Id", "CreatedAt", "DeletedAt", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, null, null, "Skoda", null },
+                    { 2, null, null, "Volkswagen", null },
+                    { 3, null, null, "Audi", null },
+                    { 4, null, null, "Seat", null },
+                    { 5, null, null, "Toyota", null },
+                    { 6, null, null, "Suzuki", null }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarMakes_Name",
+                table: "CarMakes",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarMakes");
+        }
+    }
+}
