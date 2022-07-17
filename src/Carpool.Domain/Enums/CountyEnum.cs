73531379b9221baa60e53f082b1fec0c59fc8fc2
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 15:13:37 +0200

    [County] - add County enum.

---
+++ src/Carpool.Domain/Enums/CountyEnum.cs
@@ -0,0 +1,27 @@
+﻿namespace Carpool.Domain.Enums
+{
+    public enum CountyEnum
+    {
+        BjelovarskoBilogorska = 1,
+        BrodskoPosavska = 2,
+        DubrovackoNeretvanska = 3,
+        Istarska = 4,
+        Karlovacka = 5,
+        KoprivnickoKrizevacka = 6,
+        KrapinskoZagorska = 7,
+        LickoSenjska = 8,
+        Medimurska = 9,
+        OsjeckoBaranjska = 10,
+        PozeskoSlavnoska = 11,
+        PrimorskoGoranska = 12,
+        SisackoMoslavacka = 13,
+        SplitskoDalmatinska = 14,
+        SibenskoKninska = 15,
+        Varazdinska = 16,
+        VirovitickoPodravska = 17,
+        VukovarskoSrijemska = 18,
+        Zadarska = 19,
+        Zagrebacka = 20,
+        GradZagreb = 21
+    }
+}
