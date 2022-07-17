df69a4dd48c3095d9d5f8c411287950b5b28b543
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 02:25:24 +0200

    [Migrations] - add CarMake and Color related migrations...

---
+++ src/Carpool.Infrastructure/Persistence/Migrations/20220716144809_ColorMigration.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Carpool.Infrastructure.Persistence.Migrations
+{
+    public partial class ColorMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Color",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Color", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CarMakes",
+                columns: new[] { "Id", "CreatedAt", "DeletedAt", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 7, null, null, "Chevrolet", null },
+                    { 8, null, null, "Dacia", null },
+                    { 9, null, null, "Ford", null },
+                    { 10, null, null, "Volvo", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Color",
+                columns: new[] { "Id", "CreatedAt", "DeletedAt", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, null, null, "Black", null },
+                    { 2, null, null, "Blue", null },
+                    { 3, null, null, "Brown", null },
+                    { 4, null, null, "Gold", null },
+                    { 5, null, null, "Gray", null },
+                    { 6, null, null, "Green", null },
+                    { 7, null, null, "Orange", null },
+                    { 8, null, null, "Purple", null },
+                    { 9, null, null, "Red", null },
+                    { 10, null, null, "White", null }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Color_Name",
+                table: "Color",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Color");
+
+            migrationBuilder.DeleteData(
+                table: "CarMakes",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "CarMakes",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "CarMakes",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "CarMakes",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
