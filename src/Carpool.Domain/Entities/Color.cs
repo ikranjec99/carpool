32de72dab72bf63d30b478ab8fcc75b34face700
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 02:23:27 +0200

    [Carpool.Domain] - add Color entity.

---
+++ src/Carpool.Domain/Entities/Color.cs
@@ -0,0 +1,13 @@
+﻿namespace Carpool.Domain.Entities
+{
+    public class Color
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        #region System properties
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        #endregion
+    }
+}
