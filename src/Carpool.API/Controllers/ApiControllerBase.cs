c1a9415d15f57cf89125d25272b2d0d281f7817d
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 23:46:38 +0200

    [Carpool.API] - add ApiControllerBase class.

---
+++ src/Carpool.API/Controllers/ApiControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carpool.API.Controllers
+{
+    [ApiController]
+    public class ApiControllerBase : ControllerBase
+    {
+
+    }
+}
