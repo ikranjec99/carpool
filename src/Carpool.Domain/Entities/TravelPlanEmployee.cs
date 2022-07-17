9f9183bff9e122bd484b03ad117c1504bc14a72b
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 22:36:32 +0200

    [TravelPlanEmployee] - add TravelPlanEmployee entity.

---
+++ src/Carpool.Domain/Entities/TravelPlanEmployee.cs
@@ -0,0 +1,14 @@
+﻿namespace Carpool.Domain.Entities
+{
+    public class TravelPlanEmployee
+    {
+        public Guid Id { get; set; }
+        public TravelPlan TravelPlan { get; set; }
+        public Guid TravelPlanId { get; set; }
+        public Employee Employee { get; set; }
+        public Guid EmployeeId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
