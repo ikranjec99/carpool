6cde9592ae8027039db9b4637748268a8148c971
Ivan <ivan.kranjec95@gmail.com>
2022-07-18 01:42:06 +0200

    [County dto] - add CountyDto.

---
+++ src/Carpool.Application/Counties/CountyDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Carpool.Application.Common.Mappings;
+using Carpool.Domain.Entities;
+
+namespace Carpool.Application.Counties
+{
+    public class CountyDto : IMapFrom<County>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<County, CountyDto>()
+                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name));
+        }
+    }
+}
