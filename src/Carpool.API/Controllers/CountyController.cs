5c9c2acd2bee2623b55fe11aca308bb15ae4cac8
Ivan <ivan.kranjec95@gmail.com>
2022-07-18 01:41:47 +0200

    [County controller] - add CountyController.

---
+++ src/Carpool.API/Controllers/CountyController.cs
@@ -0,0 +1,35 @@
+﻿using Carpool.Application.Common.Models;
+using Carpool.Application.Counties.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carpool.API.Controllers
+{
+    [Route("api/counties")]
+    public class CountyController : ApiControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CountyController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Get paginated counties 
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetCounties([FromQuery]QueryOptions queryOptions)
+        {
+            return Ok(await _mediator.Send(new GetPaginatedCountiesQuery(queryOptions)));
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> SearchCounties(string query)
+        {
+            return Ok(await _mediator.Send(new GetSearchedCountiesQuery(query)));
+        }
+    }
+}
