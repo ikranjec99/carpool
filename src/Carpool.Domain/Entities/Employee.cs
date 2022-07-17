1ad22857d5fe0f6e8a81ae37853942b522e00caa
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 12:59:15 +0200

    [Employee] - add Employee entity.

---
+++ src/Carpool.Domain/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Carpool.Domain.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public bool IsDriver { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
