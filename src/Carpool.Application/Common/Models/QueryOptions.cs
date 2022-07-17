f4a7ad67bb1505f251cf48b9ac15e0f77e7e775a
Ivan <ivan.kranjec95@gmail.com>
2022-07-18 01:15:34 +0200

    [Query options] - add QueryOptions class.

---
+++ src/Carpool.Application/Common/Models/QueryOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpool.Application.Common.Models
+{
+    public class QueryOptions
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+
+        public QueryOptions()
+        {
+            PageSize = PageSize > 0 ? PageSize : 10;
+            PageNumber = PageNumber > 0 ? PageNumber : 1;
+        }
+    }
+}
