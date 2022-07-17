551f22f4f3a511ccd1eddc7d94cb1ca4c5661e36
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 22:36:19 +0200

    [TravelPlan] - add TravelPlan entity.

---
+++ src/Carpool.Domain/Entities/TravelPlan.cs
@@ -0,0 +1,18 @@
+﻿namespace Carpool.Domain.Entities
+{
+    public class TravelPlan
+    {
+        public Guid Id { get; set; }
+        public Car Car { get; set; }
+        public Guid CarId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime? CreatedAt { get;  set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+
+        public virtual ICollection<TravelPlanEmployee> TravelPlanEmployees { get; set; }
+        public virtual TravelPlanStartLocation TravelPlanStartLocation { get; set; }
+        public virtual TravelPlanEndLocation TravelPlanEndLocation { get; set; }
+    }
+}
