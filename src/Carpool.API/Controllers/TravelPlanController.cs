76ad8e1c5a3981fb0aa7e4ea172a7ebb0b5cb37a
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 23:47:59 +0200

    [Travel plan controller] - add.

---
+++ src/Carpool.API/Controllers/TravelPlanController.cs
@@ -0,0 +1,28 @@
+﻿using Carpool.Application.TravelPlan.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carpool.API.Controllers
+{
+    [Route("api/travel-plan")]
+    public class TravelPlanController : ApiControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TravelPlanController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Create travel plan
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> CreateTravelPlan(CreateTravelPlanCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+    }
+}
