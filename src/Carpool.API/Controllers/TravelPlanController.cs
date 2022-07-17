8a37fd957dfc630f5cf270f5f380a4b7ab662626
Ivan <ivan.kranjec95@gmail.com>
2022-07-18 01:14:40 +0200

    [Travel plan controller] - add all CRUD routes.

--- src/Carpool.API/Controllers/TravelPlanController.cs
+++ src/Carpool.API/Controllers/TravelPlanController.cs
@@ -1,4 +1,6 @@
-﻿using Carpool.Application.TravelPlan.Commands;
+﻿using Carpool.Application.Common.Models;
+using Carpool.Application.TravelPlan.Commands;
+using Carpool.Application.TravelPlan.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +25,50 @@ namespace Carpool.API.Controllers
         public async Task<IActionResult> CreateTravelPlan(CreateTravelPlanCommand command)
         {
             return Ok(await _mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Update travel plan
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task<IActionResult> UpdateTravelPlan(UpdateTravelPlanCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Delete travel plan
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public async Task<IActionResult> DeleteTravelPlan(DeleteTravelPlanCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Get travel plan
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTravelPlan(Guid id)
+        {
+            return Ok(await _mediator.Send(new GetTravelPlanQuery { TravelPlanId = id }));
+        }
+
+        /// <summary>
+        /// Get paginated travel plans
+        /// </summary>
+        /// <param name="queryOptions"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetTravelPlans([FromQuery] QueryOptions queryOptions)
+        {
+            return Ok(await _mediator.Send(new GetTravelPlansQuery(queryOptions)));
         }
     }
 }
