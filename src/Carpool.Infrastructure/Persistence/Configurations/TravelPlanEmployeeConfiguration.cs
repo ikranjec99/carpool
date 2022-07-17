7cb67ff2f279c4ab21d7a5441cc3d5919a16bf76
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 22:37:36 +0200

    [TravelPlanEmployee] - add TravelPlanEmployee configuration.

---
+++ src/Carpool.Infrastructure/Persistence/Configurations/TravelPlanEmployeeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Carpool.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Carpool.Infrastructure.Persistence.Configurations
+{
+    public class TravelPlanEmployeeConfiguration : IEntityTypeConfiguration<TravelPlanEmployee>
+    {
+        public void Configure(EntityTypeBuilder<TravelPlanEmployee> builder)
+        {
+            builder.Property(t => t.Id)
+                .IsRequired();
+
+            builder.Property(t => t.TravelPlanId)
+                .IsRequired();
+
+            builder.Property(t => t.EmployeeId)
+                .IsRequired();
+        }
+    }
+}
