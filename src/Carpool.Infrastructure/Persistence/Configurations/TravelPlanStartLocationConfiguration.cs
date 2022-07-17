d5e0f2bb744e3bb7b75306175fddedfd0b725a22
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 22:38:11 +0200

    [TravelPlanStartLocation] - add TravelPlanStartLocation configuration.

---
+++ src/Carpool.Infrastructure/Persistence/Configurations/TravelPlanStartLocationConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Carpool.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Carpool.Infrastructure.Persistence.Configurations
+{
+    public class TravelPlanStartLocationConfiguration : IEntityTypeConfiguration<TravelPlanStartLocation>
+    {
+
+        public void Configure(EntityTypeBuilder<TravelPlanStartLocation> builder)
+        {
+            builder.Property(t => t.Id)
+                .IsRequired();
+
+            builder.Property(t => t.CityId)
+                .IsRequired();
+
+            builder.Property(t => t.TravelPlanId)
+                .IsRequired();
+        }
+    }
+}
