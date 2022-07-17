89a6b54162564f0f572d7071b675ce36e9b659b3
Ivan <ivan.kranjec95@gmail.com>
2022-07-18 01:42:57 +0200

    [TravelPlan] - update GetTravelPlanQuery.c

--- src/Carpool.Application/TravelPlan/Queries/GetTravelPlanQuery.cs
+++ src/Carpool.Application/TravelPlan/Queries/GetTravelPlanQuery.cs
@@ -1,4 +1,6 @@
-﻿using Carpool.Application.Common.Interfaces;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Carpool.Application.Common.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,24 +11,27 @@ using System.Threading.Tasks;
 
 namespace Carpool.Application.TravelPlan.Queries
 {
-    public class GetTravelPlanQuery : IRequest<Domain.Entities.TravelPlan>
+    public class GetTravelPlanQuery : IRequest<TravelPlanDto>
     {
         public Guid TravelPlanId { get; set; }
     }
 
-    public class GetTravelPlanQueryHandler : IRequestHandler<GetTravelPlanQuery, Domain.Entities.TravelPlan>
+    public class GetTravelPlanQueryHandler : IRequestHandler<GetTravelPlanQuery, TravelPlanDto>
     {
         private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public GetTravelPlanQueryHandler(IApplicationDbContext context)
+        public GetTravelPlanQueryHandler(IApplicationDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        public async Task<Domain.Entities.TravelPlan> Handle(GetTravelPlanQuery request, CancellationToken cancellationToken)
+        public async Task<TravelPlanDto> Handle(GetTravelPlanQuery request, CancellationToken cancellationToken)
         {
             var travelPlan = await _context.TravelPlans
                 .Where(x => x.Id == request.TravelPlanId)
+                .ProjectTo<TravelPlanDto>(_mapper.ConfigurationProvider)
                 .FirstOrDefaultAsync();
 
             if (travelPlan == null)
