0468451af35ac638bf4823bb4891e975d3c536cd
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 15:13:28 +0200

    [County] - add County model.

---
+++ src/Carpool.Domain/Entities/County.cs
@@ -0,0 +1,11 @@
+﻿namespace Carpool.Domain.Entities
+{
+    public class County
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
