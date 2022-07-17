072598f0a67f743024b3ffd97476233db5103213
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 22:37:07 +0200

    [TravelPlanStartLocation] - add TravelPlanStartLocation entity.

---
+++ src/Carpool.Domain/Entities/TravelPlanStartLocation.cs
@@ -0,0 +1,14 @@
+﻿namespace Carpool.Domain.Entities
+{
+    public class TravelPlanStartLocation
+    {
+        public Guid Id { get; set; }
+        public City City { get; set; }
+        public Guid CityId { get; set; }
+        public TravelPlan TravelPlan { get; set; }
+        public Guid TravelPlanId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
