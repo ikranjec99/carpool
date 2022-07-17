7c1fa34d380103b6f2ff5a040e2a486e252da4fb
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 02:24:46 +0200

    [Carpool.Infrastructure] - add ColorConfiguration.

---
+++ src/Carpool.Infrastructure/Persistence/Configurations/ColorConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Carpool.Domain.Entities;
+using Carpool.Domain.Enums.CarColor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Carpool.Infrastructure.Persistence.Configurations
+{
+    public class ColorConfiguration : IEntityTypeConfiguration<Color>
+    {
+        public void Configure(EntityTypeBuilder<Color> builder)
+        {
+            builder.Property(t => t.Id)
+                .IsRequired();
+
+            builder.Property(t => t.Name)
+                .IsRequired();
+
+            builder.HasIndex(e => e.Name)
+                .IsUnique();
+
+            var enumValues = new List<int>((int[])Enum.GetValues(typeof(ColorEnum)));
+
+            builder.HasData(enumValues.Select(t => new Color()
+            {
+                Id = t,
+                Name = ((ColorEnum)t).ToString()
+            }));
+        }
+    }
+}
