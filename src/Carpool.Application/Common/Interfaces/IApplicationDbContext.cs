e8ebd2f42f8d28b5dceee68bc23c1c7805bda2b3
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 23:47:02 +0200

    [IApplicationDbContext] - fix issue with TravelPlan...

--- src/Carpool.Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Carpool.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace Carpool.Application.Common.Interfaces
         public DbSet<Employee> Employees { get; set; }
         public DbSet<County> Counties { get; set; }
         public DbSet<City> Cities { get; set; }
-        public DbSet<TravelPlan> TravelPlans { get; set; }
+        public DbSet<Domain.Entities.TravelPlan> TravelPlans { get; set; }
         public DbSet<TravelPlanStartLocation> TravelPlanStartLocations { get; set; }
         public DbSet<TravelPlanEndLocation> TravelPlanEndLocations { get; set; }
         public DbSet<TravelPlanEmployee> TravelPlanEmployees { get; set; }
