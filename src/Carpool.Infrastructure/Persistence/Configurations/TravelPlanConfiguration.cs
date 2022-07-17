35ea7b6d215bfa69c56b24695d910ddc40a30d9d
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 22:37:25 +0200

    [TravelPlan] - add TravelPlan configuration.

---
+++ src/Carpool.Infrastructure/Persistence/Configurations/TravelPlanConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Carpool.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpool.Infrastructure.Persistence.Configurations
+{
+    public class TravelPlanConfiguration : IEntityTypeConfiguration<TravelPlan>
+    {
+        public void Configure(EntityTypeBuilder<TravelPlan> builder)
+        {
+            builder.Property(t => t.Id)
+                .IsRequired();
+
+            builder.Property(t => t.CarId)
+                .IsRequired();
+
+            builder.Property(t => t.StartDate)
+                .IsRequired();
+
+            builder.Property(t => t.EndDate)
+                .IsRequired();
+        }
+    }
+}
