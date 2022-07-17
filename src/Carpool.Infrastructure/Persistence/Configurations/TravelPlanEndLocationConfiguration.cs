9c2ee3f0c9fa68dd12f4467d813f710cccfa9506
Ivan <ivan.kranjec95@gmail.com>
2022-07-17 22:37:54 +0200

    [TravelPlanEndLocation] - add TravelPlanEndLocation configuration.

---
+++ src/Carpool.Infrastructure/Persistence/Configurations/TravelPlanEndLocationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Carpool.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Carpool.Infrastructure.Persistence.Configurations
+{
+    public class TravelPlanEndLocationConfiguration : IEntityTypeConfiguration<TravelPlanEndLocation>
+    {
+        public void Configure(EntityTypeBuilder<TravelPlanEndLocation> builder)
+        {
+            builder.Property(t => t.Id)
+                .IsRequired();
+
+            builder.Property(t => t.CityId)
+                .IsRequired();
+
+            builder.Property(t => t.TravelPlanId)
+                .IsRequired();
+        }
+    }
+}
