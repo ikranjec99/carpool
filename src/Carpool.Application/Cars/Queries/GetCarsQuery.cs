85e9ca68e24d84cc2042dd6ad454ffd7c1026f50
Ivan <ivan.kranjec95@gmail.com>
2022-07-18 23:16:47 +0200

    [Cars] - add GetCarsQuery.

---
+++ src/Carpool.Application/Cars/Queries/GetCarsQuery.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Carpool.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Carpool.Application.Cars.Queries
+{
+    public class GetCarsQuery : IRequest<IList<CarDto>>
+    {
+
+    }
+
+    public class GetCarsQueryHandler : IRequestHandler<GetCarsQuery, IList<CarDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetCarsQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IList<CarDto>> Handle(GetCarsQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Cars
+                .ProjectTo<CarDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+    }
+}
