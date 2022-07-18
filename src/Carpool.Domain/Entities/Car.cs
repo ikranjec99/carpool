2fa8e01eb41983427e56a1ec27e8cc3d9ab01b02
Ivan <ivan.kranjec95@gmail.com>
2022-07-18 23:15:02 +0200

    [Car] - update Car entity, add TravelPlan virtual property.

--- src/Carpool.Domain/Entities/Car.cs
+++ src/Carpool.Domain/Entities/Car.cs
@@ -14,5 +14,7 @@
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
+
+        public virtual TravelPlan TravelPlan { get; set; }
     }
 }
