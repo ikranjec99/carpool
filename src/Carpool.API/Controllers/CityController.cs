a6ed09268df0adc92c206129b9d6c5b2f539607e
Ivan <ivan.kranjec95@gmail.com>
2022-07-18 23:18:09 +0200

    [City] - add CityController.

---
+++ src/Carpool.API/Controllers/CityController.cs
@@ -0,0 +1,28 @@
+﻿using Carpool.Application.Cities.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carpool.API.Controllers
+{
+    [Route("api/cities")]
+    public class CityController : ApiControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CityController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Get list of cities by search term 
+        /// </summary>
+        /// <param name="queryOptions"></param>
+        /// <returns></returns>
+        [HttpGet("search")]
+        public async Task<IActionResult> GetCities(string searchTerm)
+        {
+            return Ok(await _mediator.Send(new GetSearchedCitiesQuery(searchTerm)));
+        }
+    }
+}
