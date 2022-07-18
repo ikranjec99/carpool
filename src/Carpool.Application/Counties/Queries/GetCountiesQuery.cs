14be8b9ee697b1c613ce4f5704811cb0a2f2f5f4
Ivan <ivan.kranjec95@gmail.com>
2022-07-18 23:15:54 +0200

    [Counties] - add GetCountiesQuery.

---
+++ src/Carpool.Application/Counties/Queries/GetCountiesQuery.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Carpool.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Carpool.Application.Counties.Queries
+{
+    public class GetCountiesQuery : IRequest<IList<CountyDto>>
+    {
+        
+    }
+
+    public class GetCountiesQueryHandler : IRequestHandler<GetCountiesQuery, IList<CountyDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetCountiesQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IList<CountyDto>> Handle(GetCountiesQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Counties
+                .ProjectTo<CountyDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+    }
+
+}
