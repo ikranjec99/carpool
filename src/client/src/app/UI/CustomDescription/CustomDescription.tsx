03261645692894f562de4c511ac4da3b4fec630d
Ivan <ivan.kranjec95@gmail.com>
2022-07-19 00:45:10 +0200

    [Client] - add CustomDescription component.

---
+++ src/client/src/app/UI/CustomDescription/CustomDescription.tsx
@@ -0,0 +1,5 @@
+import { CustomDescriptionProps } from "./CustomDescriptionProps";
+
+export const CustomDescription = ({ description }: CustomDescriptionProps) => {
+    return <div className={'default-description'}>{description}</div>
+}
