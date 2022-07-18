2f9046c43744b64e031dc055d63655491eb86dc9
Ivan <ivan.kranjec95@gmail.com>
2022-07-18 23:17:54 +0200

    [Car] - add CarController.

---
+++ src/Carpool.API/Controllers/CarController.cs
@@ -0,0 +1,41 @@
+﻿using Carpool.Application.Cars.Queries;
+using Carpool.Application.Common.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carpool.API.Controllers
+{
+    [Route("api/cars")]
+    public class CarController : ApiControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CarController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Get list of all cars
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetCars()
+        {
+            return Ok(await _mediator.Send(new GetCarsQuery()));
+        }
+
+
+        /// <summary>
+        /// Get list of all available cars
+        /// </summary>
+        /// <param name="StartDate"></param>
+        /// <param name="EndDate"></param>
+        /// <returns></returns>
+        [HttpGet("available")]
+        public async Task<IActionResult> GetAvailableCars(GetAvailableCarsQuery query)
+        {
+            return Ok(await _mediator.Send(query));
+        }
+    }
+}
