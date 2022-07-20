aedb853e7473fba2e4a950e94e3cd683d4e4e9d5
Ivan <ivan.kranjec95@gmail.com>
2022-07-21 00:06:46 +0200

    [CarDto] - add CarDto to client.

---
+++ src/client/src/app/Interfaces/Dtos/CarDto.tsx
@@ -0,0 +1,11 @@
+export interface CarDto {
+   id: string;
+   name: string;
+   carMakeName: string;
+   carMakeId: number;
+   carModel: string;
+   colorName: string;
+   colorId: number;
+   plates: string;
+   numberOfSeats: number;
+}
