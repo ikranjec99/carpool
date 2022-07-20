91a24b075518dd0a8d89e5f94c48e51aafd27c0a
Ivan <ivan.kranjec95@gmail.com>
2022-07-21 00:02:19 +0200

    [Router] - update router.

--- src/client/src/app/Router/Router.tsx
+++ src/client/src/app/Router/Router.tsx
@@ -4,36 +4,22 @@ import { AddIcon } from "../UI/CustomIcons/AddIcon/AddIcon";
 import { ListIcon } from "../UI/CustomIcons/ListIcon/ListIcon";
 import { HomeIcon } from "../UI/CustomIcons/HomeIcon/HomeIcon";
 import { EditIcon } from "../UI/CustomIcons/EditIcon/EditIcon";
+import { Home } from "../Pages/Home/Home";
+import { TravelPlans } from "../Pages/TravelPlans/TravelPlans";
 
 export const Router : RouterProps[] = [
   {
     name: "Home",
     icon: <HomeIcon size={20}/>,
     route: "",
-    component: <>Welcome home!</>,
+    component: <Home description="Welcome home!" />,
     visibleInNavigation: true
   },
   {
-    name: "Activities",
+    name: "Travel plans",
     icon: <ListIcon />,
-    route: "/activities",
-    component: <>Activities</>,
-    visibleInNavigation: true,
-    children: [
-      {
-        name: "Create activity",
-        icon: <AddIcon/>,
-        route: "/activities/editor",
-        component: <>Activities editor</>,
-        visibleInNavigation: true
-      },
-      {
-        name: "Edit activity",
-        icon: <EditIcon/>,
-        route: "/activities/editor/:activityId",
-        visibleInNavigation: false,
-        component: <>Edit activity</>
-      }
-    ]
+    route: "/travel-plans",
+    component: <TravelPlans />,
+    visibleInNavigation: true
   }
 ];
